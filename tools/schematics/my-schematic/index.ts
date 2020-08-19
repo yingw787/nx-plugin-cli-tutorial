776e4f8e00fa2572d4cdb2cd6c29703f703a2d88
Ying Wang <yingw787.personal@gmail.com>
2020-08-19 11:53:08 -0400

    Update index.ts pasting from tutorial.

--- tools/schematics/my-schematic/index.ts
+++ tools/schematics/my-schematic/index.ts
@@ -1,9 +1,39 @@
-import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import {
+  apply,
+  chain,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  externalSchematic,
+} from '@angular-devkit/schematics';
+import { getProjectConfig } from '@nrwl/workspace';
+
+function generateLibrary(schema: any): Rule {
+  return externalSchematic('@nrwl/workspace', 'lib', {
+    name: schema.name,
+  });
+}
+
+function generateFiles(schema: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info('adding NOTES.md to lib');
+
+    const templateSource = apply(url('./files'), [
+      move(getProjectConfig(tree, schema.name).root),
+    ]);
+
+    return chain([mergeWith(templateSource)])(tree, context);
+  };
+}
 
 export default function (schema: any): Rule {
-  return chain([
-    externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name,
-    }),
-  ]);
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([generateLibrary(schema), generateFiles(schema)])(
+      tree,
+      context
+    );
+  };
 }
