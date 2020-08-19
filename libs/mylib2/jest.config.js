2912f97d5fb18bbec826e305576eae5da4f417da
Ying Wang <yingw787.personal@gmail.com>
2020-08-19 11:55:40 -0400

    Add / update misc files runcmd 'nx workspace-schematic my-schematic mylib2' primary difference is mylib2 creates NOTES.md in root directory.

---
+++ libs/mylib2/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'mylib2',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/mylib2',
+};
